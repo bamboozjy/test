ea98e2d46a09cfff655a97f2cc2337007fb2c4ae
bamboozjy <1308205052@qq.com>
2018-07-16 15:24:11 +0800

    add new fn

--- page/app.js
+++ page/app.js
@@ -26,7 +26,7 @@ App({
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                this.userInfoReadyCallback(res);
               }
             }
           })
