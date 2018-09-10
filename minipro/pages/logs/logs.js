0cbde57a56b2d5833027d9b836c1d7c62deccb22
bamboozjy <1308205052@qq.com>
2018-09-10 19:46:21 +0800

    213

--- minipro/pages/logs/logs.js
+++ minipro/pages/logs/logs.js
@@ -3,24 +3,14 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-<<<<<<< .merge_file_gTNPc6
-    logs: [121111345]
-=======
-    logs: ['1232vcvbn1345']
->>>>>>> .merge_file_GuVoix
+    logs: [12111134522222]
   },
   onLoad: function () {
     console.log('1vedw23')
     this.setData({
-<<<<<<< .merge_file_gTNPc6
-      logs: (wx.getStorageSync('logswe')).map(log => {
-        conosle.log('nihac o13')
-        return util.formatTime(new Date(loesdg))
-=======
       logs: (wx.getStorageSync('logsqq')).map(log => {
-        conosle.log('ni34253t hao')
+        conosle.log('ni34253t2222222hao')
         return util.formatTime(new Date(log))
->>>>>>> .merge_file_GuVoix
       })
     })
   }
