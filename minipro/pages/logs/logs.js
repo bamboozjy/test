5f79cb67820e0a2083c9993a1531c2922a0e44d0
bamboozjy <1308205052@qq.com>
2018-09-10 18:35:55 +0800

    1234

--- minipro/pages/logs/logs.js
+++ minipro/pages/logs/logs.js
@@ -6,6 +6,7 @@ Page({
     logs: []
   },
   onLoad: function () {
+    console.log(12)
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
