86211ef0159e255c27d65938aaaf13f1f8ae1823
bamboozjy <1308205052@qq.com>
2018-09-11 10:02:49 +0800

    aaa

--- page/pages/logs/logs.js
+++ page/pages/logs/logs.js
@@ -3,15 +3,11 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: ['365664']
+    logs: ['aaaaaa']
   },
   onLoad: function () {
-    console.log('test2');
     this.setData({
-      logs: (wx.getStorageSync('logsaa') || ['121']).map(log => {
-        conosle.log('nihaobaqeg')
-        return util.formatTime(new Date(logq))
-      })
+      msg:'aaaaaa'
     })
   }
 })
